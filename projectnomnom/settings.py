5c33f5b3b57479bae30bf3e5ae7a06650c43c021
shakalandro <shakalandro@gmail.com>
2011-12-20 02:37:48 -0800

    initial seed for heroku

--- projectnomnom/settings.py
+++ projectnomnom/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/Users/shakalandro/Code/Projects/ProjectNomNom2/recipes.csv',                      # Or path to database file if using sqlite3.
+        'NAME': '/Users/shakalandro/Code/Projects/ProjectNomNom2/projectnomnomrecipes.csv',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
