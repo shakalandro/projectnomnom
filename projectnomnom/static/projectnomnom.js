2c1636c83328cb69e2f9544e441e72be989e449b
shakalandro <shakalandro@gmail.com>
2011-12-23 15:11:00 -0800

    added fb share button

--- projectnomnom/static/projectnomnom.js
+++ projectnomnom/static/projectnomnom.js
@@ -1,21 +1,12 @@
-/*
 $(document).ready(function() {
-	window.fbAsyncInit = function() {
-        FB.init({appId: '161204603981737', status: true, cookie: true,
-        		 xfbml: true, oauth: true});
-        FB.Event.subscribe('auth.login',
-        	function(response) {
-            	FB.api('/me', function(data) {
-            		alert(data.name);
-            	});
-            }
-        );
-    };
-    (function(d){
-       var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
-       js = d.createElement('script'); js.id = id; js.async = true;
-       js.src = "//connect.facebook.net/en_US/all.js";
-       d.getElementsByTagName('head')[0].appendChild(js);
-    }(document));
+	(function(d){
+		var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
+		js = d.createElement('script'); js.id = id; js.async = true;
+		if (document.URL.indexOf('127.0.0.1') != -1 || document.URL.indexOf('localhost') != -1) {
+			js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=275585199158118";
+		} else {
+			js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=161204603981737";
+		}
+     	d.getElementsByTagName('head')[0].appendChild(js);
+	}(document));
 });
-*/
